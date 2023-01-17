87f3b50cd21843ee5dd6a4e9dd9d84d98b8f5ee5
Ibrahim Balta <baltaibrahim584@gmail.com>
2023-01-17 22:44:09 +0300

    story eklendi

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
+List story = [
+  {"name": "Hikaye Ekle", "image": "assets/add.png"},
+  {"name": "Sudenaz", "image": "assets/1.png"},
+  {"name": "Havva", "image": "assets/2.png"},
+  {"name": "İbrahim", "image": "assets/3.png"},
+];
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -16,7 +23,7 @@ class _HomePageState extends State<HomePage> {
         children: [
           Container(
             width: 428,
-            height: 256,
+            height: 295,
             decoration: const BoxDecoration(
                 borderRadius: BorderRadius.only(
                     bottomLeft: Radius.circular(42),
@@ -44,9 +51,8 @@ class _HomePageState extends State<HomePage> {
                               width: 73,
                               height: 49,
                               decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(28)
-                              ),
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(28)),
                               child: Image.asset("assets/sms.png"),
                             )
                           ],
@@ -54,7 +60,71 @@ class _HomePageState extends State<HomePage> {
                       )
                     ],
                   ),
-                )
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 130,
+                  child: Expanded(
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: story.length,
+                      itemBuilder: ((context, index) {
+                        return Column(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.symmetric(horizontal: 10),
+                              width: 100,
+                              height: 84,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(38),
+                                gradient: LinearGradient(
+                                  begin: Alignment.bottomCenter,
+                                  end: Alignment.topCenter,
+                                  colors: [
+                                    Color.fromARGB(255, 81, 142, 255),
+                                    Color.fromARGB(255, 155, 92, 255)
+                                  ],
+                                ),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.all(5),
+                                child: Container(
+                                  width: 100,
+                                  height: 84,
+                                  decoration: BoxDecoration(
+                                    color: Color.fromARGB(255, 66, 66, 66),
+                                    borderRadius: BorderRadius.circular(28),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        color: Color.fromARGB(255, 66, 66, 66),
+                                        borderRadius: BorderRadius.circular(38),
+                                      ),
+                                      width: 75,
+                                      height: 65,
+                                      child: Image.asset(
+                                          "${story[index]["image"]}"),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              "${story[index]["name"]}",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 15,
+                                  fontFamily: "RobotoCondensed Pro"),
+                            )
+                          ],
+                        );
+                      }),
+                    ),
+                  ),
+                ),
               ],
             ),
           )
